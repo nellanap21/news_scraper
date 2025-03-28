e6dd1e86c62174dc0bc25ace198ce101ad0f45e6
nellanap21 <ap543@student.london.ac.uk>
2025-03-27 18:08:58 -0700

    add CNN page scraper

--- src/main_local.py
+++ src/main_local.py
@@ -3,6 +3,7 @@ import requests  # Version 2.27.1
 from bs4 import BeautifulSoup  # Version 4.11.1
 import pandas as pd  # Version 1.4.2
 from datetime import date  # Built-in Python library
+import numpy as np
 import re
 
 # Code refactor
@@ -143,6 +144,28 @@ def parse_fox_page(soup):
 def remove_all_caps(string):
     return re.sub(r'\b[A-Z]{2}\w*+\b', '', string)
 
+
+def parse_cnn_page(soup):
+    # parse article headline
+    h1 = soup.find("h1")
+    if h1:
+        headline = h1.get_text(strip=True)
+    else:
+        print("No h1 tag")
+
+    # parse article text
+    article_content = soup.find("div", class_="article__content").find_all("p")
+
+    content = ""
+    for paragraph in article_content:
+        content = content + " " + paragraph.get_text(strip=True)
+
+    # replace non-breaking space with actual space
+    content = content.replace('\xa0', ' ')
+
+    return headline, content
+
+
 def scrape_fox_pages():
     # create lists of equal length to hold data
     companies = []
@@ -187,7 +210,7 @@ def scrape_fox_pages():
             headline, content = parse_fox_page(soup)
             company = "Fox"
         elif domain == "cnn":
-            headline, content = parseCnnPage(soup)
+            headline, content = parse_cnn_page(soup)
             company = "CNN"
         else:
             print("URL is not from Fox or CNN", url)
@@ -204,6 +227,7 @@ def scrape_fox_pages():
     if len(raw_data["company"]) == len(raw_data["headline"]) & len(raw_data["headline"]) == len(raw_data["article"]):
         data = pd.DataFrame(raw_data)
 
+    # remove all caps from article
     data.loc[:,"articleCleaned"] = data["article"].apply(remove_all_caps)
     data.loc[:,"article"] = data["articleCleaned"]
     data.drop("articleCleaned", axis=1, inplace=True)
@@ -211,8 +235,65 @@ def scrape_fox_pages():
     csv_data_name = today.strftime("%Y-%m-%d") + '-fox-data.csv'
     data.to_csv(csv_data_name, index=False)
 
+def scrape_cnn_pages():
+    today = date.today()
+    csv_link_name = today.strftime("%Y-%m-%d") + '-cnn-links.csv'
+    links = pd.read_csv(csv_link_name)
+
+    # create a new column called company from the link
+    links["domain"] = ""
+
+    for i in range(0, len(links)):
+        start = (links.iloc[i].url.find("www.")) + 4
+        end = links.iloc[i].url.find(".com")
+        links.at[i, "domain"] = links.iloc[i].url[start:end]
+
+    # create lists of equal length to hold data
+    companies = []
+    headlines = []
+    articles = []
+
+    for i in range(0, len(links)):
+
+        # get the url of the row
+        url = links.at[i, "url"]
+
+        # make HTTP request and get HTML
+        soup = request_html(url)
+
+        # if there was an error in requestHtml, continue to next
+        if soup == "error":
+            print("Error with HTTP request", url)
+            continue
+
+        # get the domain of the row
+        domain = links.at[i, "domain"]
+
+        # depending on the domain, parse the HTML to obtain the headline and content
+        if domain == "foxnews":
+            headline, content = parse_fox_page(soup)
+            company = "Fox"
+        elif domain == "cnn":
+            headline, content = parse_cnn_page(soup)
+            company = "CNN"
+        else:
+            print("URL is not from Fox or CNN", url)
+            continue
+
+        companies.append(company)
+        headlines.append(headline)
+        articles.append(content)
+
+    # create dictionary of equal length lists
+    raw_data = {"company": companies, "headline": headlines, "article": articles}
+
+    # check to make sure dictionary of equal-length lists
+    if len(raw_data["company"]) == len(raw_data["headline"]) & len(raw_data["headline"]) == len(raw_data["article"]):
+        data = pd.DataFrame(raw_data)
 
 
+    csv_data_name = today.strftime("%Y-%m-%d") + '-cnn-data.csv'
+    data.to_csv(csv_data_name, index=False)
 
 def main():
     """
@@ -234,7 +315,9 @@ def main():
     # scrape_cnn_links()
     # logger.info('Links retrieved')
 
-
+    # Scrape CNN pages
+    scrape_cnn_pages()
+    logger.info('CNN Data retrieved')
 
 
 
